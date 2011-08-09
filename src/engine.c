ce635886d031dfd7a23bfca79d1d919a61214b21
Peng Huang <shawn.p.huang@gmail.com>
2011-08-09 18:25:07 -0400

    wip

--- src/engine.c
+++ src/engine.c
@@ -5,12 +5,73 @@ typedef struct _IBusXkbLayoutEngineClass IBusXkbLayoutEngineClass;
 
 struct _IBusXkbLayoutEngine {
     IBusEngine parent;
+
+    guint      compose_buffer[8];
+    gunichar   tentative_match;
+    gint       tentative_match_len;
+
+    guint      in_hex_sequence : 1;
+    guint      modifiers_dropped : 1;
 };
 
 struct _IBusXkbLayoutEngineClass {
     IBusEngineClass parent;
 };
 
+struct _GtkComposeTable GtkComposeTable;
+typedef struct _GtkComposeTableCompact GtkComposeTableCompact;
+
+struct _GtkComposeTable 
+{
+  const guint16 *data;
+  gint max_seq_len;
+  gint n_seqs;
+};
+
+struct _GtkComposeTableCompact
+{
+  const guint16 *data;
+  gint max_seq_len;
+  gint n_index_size;
+  gint n_index_stride;
+};
+
+/* This file contains the table of the compose sequences, 
+ * static const guint16 gtk_compose_seqs_compact[] = {}
+ * IT is generated from the compose-parse.py script.
+ */
+#include "gtkimcontextsimpleseqs.h"
+
+/* From the values below, the value 23 means the number of different first keysyms 
+ * that exist in the Compose file (from Xorg). When running compose-parse.py without 
+ * parameters, you get the count that you can put here. Needed when updating the
+ * gtkimcontextsimpleseqs.h header file (contains the compose sequences).
+ */
+static const GtkComposeTableCompact gtk_compose_table_compact = {
+  gtk_compose_seqs_compact,
+  5,
+  24,
+  6
+};
+
+static const guint16 gtk_compose_ignore[] = {
+  IBUS_Shift_L,
+  IBUS_Shift_R,
+  IBUS_Control_L,
+  IBUS_Control_R,
+  IBUS_Caps_Lock,
+  IBUS_Shift_Lock,
+  IBUS_Meta_L,
+  IBUS_Meta_R,
+  IBUS_Alt_L,
+  IBUS_Alt_R,
+  IBUS_Super_L,
+  IBUS_Super_R,
+  IBUS_Hyper_L,
+  IBUS_Hyper_R,
+  IBUS_Mode_switch,
+  IBUS_ISO_Level3_Shift
+};
 /* functions prototype */
 static void     ibus_xkb_layout_engine_class_init       (IBusXkbLayoutEngineClass   *klass);
 static void     ibus_xkb_layout_engine_init             (IBusXkbLayoutEngine        *engine);
@@ -44,12 +105,218 @@ ibus_xkb_layout_engine_destroy (IBusXkbLayoutEngine *xkb_layout)
     ((IBusObjectClass *) ibus_xkb_layout_engine_parent_class)->destroy ((IBusObject *)xkb_layout);
 }
 
-static gboolean
+#define HEX_MOD_MASK (IBUS_CONTROL_MASK | IBUS_SHIFT_MASK)
+
+gboolean
 ibus_xkb_layout_engine_process_key_event (IBusEngine *engine,
                                           guint       keyval,
                                           guint       keycode,
                                           guint       modifiers)
 {
-    /* TODO: Support Compose/Dead keys */
-    return FALSE;
+    IBusXkbLayoutEngine *xkbengine = (IBusXkbLayoutEngine *)engine;
+    GSList *tmp_list;  
+    gint n_compose = 0;
+    gboolean have_hex_mods;
+    gboolean is_hex_start;
+    gboolean is_hex_end;
+    gboolean is_backspace;
+    gboolean is_escape;
+    guint hex_keyval;
+    gint i;
+
+    while (xkbengine->compose_buffer[n_compose] != 0)
+        n_compose++;
+
+    if (modifiers & IBUS_RELEASE_MASK) {
+        if (xkbengine->in_hex_sequence &&
+            (keyval == IBUS_Control_L || keyval == IBUS_Control_R ||
+             keyval == IBUS_Shift_L || keyval == IBUS_Shift_R)) {
+            if (xkbengine->tentative_match &&
+                g_unichar_validate (xkbengine->tentative_match)) {
+                ibus_engine_commit_text (
+                      engine,
+                      ibus_text_new_from_unichar (xkbengine->tentative_match));
+            }
+            else if (n_compose == 0) {
+                xkbengine->modifiers_dropped = TRUE;
+            }
+            else {
+                /* invalid hex sequence */
+                /* beep_window (event->window); */
+                xkbengine->tentative_match = 0;
+                xkbengine->in_hex_sequence = FALSE;
+                xkbengine->compose_buffer[0] = 0;
+            
+                ibus_engine_hide_preedit_text (engine);
+            }
+            
+            return TRUE;
+        }
+        else
+            return FALSE;
+    }
+
+    /* Ignore modifier key presses */
+    for (i = 0; i < G_N_ELEMENTS (gtk_compose_ignore); i++)
+        if (keyval == gtk_compose_ignore[i])
+            return FALSE;
+
+    if (xkbengine->in_hex_sequence && xkbengine->modifiers_dropped)
+        have_hex_mods = TRUE;
+    else
+        have_hex_mods = (modifiers & (HEX_MOD_MASK)) == HEX_MOD_MASK;
+
+    is_hex_start = keyval == IBUS_U;
+    is_hex_end = (keyval == IBUS_space ||
+                  keyval == IBUS_KP_Space ||
+                  keyval == IBUS_Return ||
+                  keyval == IBUS_ISO_Enter ||
+                  keyval == IBUS_KP_Enter);
+    is_backspace = keyval == IBUS_BackSpace;
+    is_escape = keyval == IBUS_Escape;
+    hex_keyval = keyval;
+
+    /* If we are already in a non-hex sequence, or
+     * this keystroke is not hex modifiers + hex digit, don't filter
+     * key events with accelerator modifiers held down. We only treat
+     * Control and Alt as accel modifiers here, since Super, Hyper and
+     * Meta are often co-located with Mode_Switch, Multi_Key or
+     * ISO_Level3_Switch.
+     */
+    if (!have_hex_mods ||
+        (n_compose > 0 && !xkbengine->in_hex_sequence) ||
+        (n_compose == 0 && !xkbengine->in_hex_sequence && !is_hex_start) ||
+        (xkbengine->in_hex_sequence && !hex_keyval &&
+         !is_hex_start && !is_hex_end && !is_escape && !is_backspace)) {
+        if (modifiers & (IBUS_MOD1_MASK | IBUS_CONTROL_MASK) ||
+            (xkbengine->in_hex_sequence && xkbengine->modifiers_dropped &&
+             (keyval == IBUS_Return ||
+              keyval == IBUS_ISO_Enter ||
+              keyval == IBUS_KP_Enter))) {
+              return FALSE;
+        }
+    }
+
+    /* Handle backspace */
+    if (xkbengine->in_hex_sequence && have_hex_mods && is_backspace) {
+        if (n_compose > 0) {
+	    n_compose--;
+            xkbengine->compose_buffer[n_compose] = 0;
+            check_hex (context_simple, n_compose);
+	}
+        else {
+	    xkbengine->in_hex_sequence = FALSE;
+	}
+
+        // FIXME g_signal_emit_by_name (context_simple, "preedit-changed");
+
+        if (!xkbengine->in_hex_sequence)
+            ibus_engine_hide_preedit_text (engine);
+        
+        return TRUE;
+    }
+
+    /* Check for hex sequence restart */
+    if (xkbengine->in_hex_sequence && have_hex_mods && is_hex_start) {
+        if (xkbengine->tentative_match &&
+            g_unichar_validate (xkbengine->tentative_match)) {
+            ibus_engine_commit_text (
+                    engine,
+                    ibus_text_new_from_unichar (xkbengine->tentative_match));
+	    xkbengine->compose_buffer[0] = 0;
+	}
+        else {
+	    /* invalid hex sequence */
+	    if (n_compose > 0)
+	        // FIXME beep_window (event->window);
+                ;
+	  
+	    xkbengine->tentative_match = 0;
+	    xkbengine->in_hex_sequence = FALSE;
+	    xkbengine->compose_buffer[0] = 0;
+	}
+    }
+  
+    /* Check for hex sequence start */
+    if (!xkbengine->in_hex_sequence && have_hex_mods && is_hex_start) {
+        xkbengine->compose_buffer[0] = 0;
+        xkbengine->in_hex_sequence = TRUE;
+        xkbengine->modifiers_dropped = FALSE;
+        xkbengine->tentative_match = 0;
+        
+        // FIXME
+        // g_signal_emit_by_name (context_simple, "preedit-start");
+        // g_signal_emit_by_name (context_simple, "preedit-changed");
+  
+        return TRUE;
+    }
+  
+    /* Then, check for compose sequences */
+    if (xkbengine->in_hex_sequence) {
+        if (hex_keyval)
+            xkbengine->compose_buffer[n_compose++] = hex_keyval;
+        else if (is_escape) {
+            // FIXME
+	    gtk_im_context_simple_reset (context);
+	  
+	    return TRUE;
+	}
+        else if (!is_hex_end) {
+          // FIXME
+	  /* non-hex character in hex sequence */
+	  // beep_window (event->window);
+	  
+	  return TRUE;
+	}
+    }
+    else
+        xkbengine->compose_buffer[n_compose++] = keyval;
+
+    xkbengine->compose_buffer[n_compose] = 0;
+
+    if (xkbengine->in_hex_sequence) {
+        /* If the modifiers are still held down, consider the sequence again */
+        if (have_hex_mods) {
+            /* space or return ends the sequence, and we eat the key */
+            if (n_compose > 0 && is_hex_end) {
+	        if (xkbengine->tentative_match &&
+		    g_unichar_validate (xkbengine->tentative_match)) {
+                    ibus_engine_commit_text (
+                            engine,
+                            ibus_text_new_from_unichar (xkbengine->tentative_match));
+		    xkbengine->compose_buffer[0] = 0;
+		}
+	        else {
+                    // FIXME
+		    /* invalid hex sequence */
+		    // beep_window (event->window);
+                    xkbengine->tentative_match = 0;
+                    xkbengine->in_hex_sequence = FALSE;
+                    xkbengine->compose_buffer[0] = 0;
+		}
+            }
+            else if (!check_hex (context_simple, n_compose))
+                // FIXME
+	        // beep_window (event->window);
+                ;
+	  
+            // FIXME
+	    // g_signal_emit_by_name (context_simple, "preedit-changed");
+
+	    if (!xkbengine->in_hex_sequence)
+                ibus_engine_hide_preedit_text (engine);
+	    return TRUE;
+        }
+    }
+    else {
+        // TODO CONT
+        if (check_compact_table (context_simple, &gtk_compose_table_compact, n_compose))
+            return TRUE;
+  
+        if (check_algorithmically (context_simple, n_compose))
+	    return TRUE;
+    }
+  
+    /* The current compose_buffer doesn't match anything */
+    return no_sequence_matches (context_simple, n_compose, event);
 }
