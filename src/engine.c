91b7cb24ce80dbf21acfef4801532a36f1ee9cba
Peng Huang <shawn.p.huang@gmail.com>
2011-08-10 23:07:31 -0400

    wip

--- src/engine.c
+++ src/engine.c
@@ -409,7 +409,7 @@ check_normalize_nfc (gunichar* combination_buffer, gint n_compose)
     for (i = 0; i < n_combinations; i++ ) {
         g_unicode_canonical_ordering (combination_buffer_temp, n_compose);
         combination_utf8_temp = g_ucs4_to_utf8 (combination_buffer_temp, -1, NULL, NULL, NULL);
-        nfc_temp = g_utf8_normalize (combination_utf8_temp, -1, G_NORMALIZE_NFC);               
+        nfc_temp = g_utf8_normalize (combination_utf8_temp, -1, G_NORMALIZE_NFC);
 
         if (g_utf8_strlen (nfc_temp, -1) == 1) {
             memcpy (combination_buffer, combination_buffer_temp, MAX_COMPOSE_LEN * sizeof (gunichar) );
